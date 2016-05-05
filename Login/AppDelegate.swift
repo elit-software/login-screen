3c7bbd5500d56ec244a25d0caea5433631e1e6bc
tiagomartinho <tiago.martinho@xpeppers.com>
2016-05-05 18:46:31 +0200

    adds end to end ui tests

--- Login/AppDelegate.swift
+++ Login/AppDelegate.swift
@@ -4,4 +4,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        disableAnimationsForTests()
+        return true
+    }
+
+    func disableAnimationsForTests() {
+        if NSProcessInfo.processInfo().environment["animations"] == "0" {
+            UIView.setAnimationsEnabled(false)
+        }
+    }
 }
--- LoginUITests/LoginUITests.swift
+++ LoginUITests/LoginUITests.swift
@@ -2,6 +2,47 @@ import XCTest
 
 class LoginUITests: XCTestCase {
 
-    func testExample() {
+    let application = XCUIApplication()
+
+    var usernameTextField: XCUIElement?
+    var passwordTextField: XCUIElement?
+    var loginButton: XCUIElement?
+
+    private var applicationLaunched = false
+
+    override func setUp() {
+        super.setUp()
+        application.launch()
+
+        usernameTextField = application.textFields["username"]
+        passwordTextField = application.textFields["password"]
+        loginButton = application.buttons["login"]
+    }
+
+    private func launchApp() {
+        if !applicationLaunched {
+            applicationLaunched = true
+            continueAfterFailure = false
+            application.launchEnvironment = ["animations": "0"]
+            application.launch()
+            sleep(1)
+        }
+    }
+
+    func testLogin() {
+        insertText("username", inTextField: usernameTextField)
+        insertText("password", inTextField: passwordTextField)
+        loginButton?.tap()
+
+        sleep(1)
+
+        let label = application.staticTexts.elementMatchingType(.Any, identifier: "label")
+        XCTAssertEqual("42", label.label)
+    }
+
+    func insertText(text: String, inTextField textField: XCUIElement?) {
+        textField?.tap()
+        textField?.tap()
+        textField?.typeText(text)
     }
 }
