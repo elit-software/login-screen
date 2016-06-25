1389001f8faa6a5b7248eb11cc29cfe79e8a3613
tiagomartinho <tiago.martinho@xpeppers.com>
2016-06-25 13:04:33 +0200

    extracts LoginView methods to extension of LoginController in swift command line app

---
+++ CrossPlatform/Sources/LoginController+LoginView.swift
@@ -0,0 +1,32 @@
+extension LoginController: LoginView {
+
+    var credentials: Credentials {
+        return Credentials(username: username, password: password)
+    }
+    
+    func showUsernameIsEmpty() {
+        print("Username Is Empty")
+    }
+
+    func showPasswordIsEmpty() {
+        print("Password Is Empty")
+    }
+
+    func showCredentialsAreValid() {
+        print("Credentials Are Valid")
+        let controller = MainController()
+        controller.display()
+    }
+
+    func showCredentialsAreNotValid() {
+        print("Credentials Are Not Valid")
+    }
+
+    func showLoadingInterface() {
+        print("Loading")
+    }
+
+    func hideLoadingInterface() {
+        print("Loading Complete")
+    }
+}
--- CrossPlatform/Sources/LoginController.swift
+++ CrossPlatform/Sources/LoginController.swift
@@ -1,4 +1,4 @@
-class LoginController: LoginView {
+class LoginController {
 
     let username: String
     let password: String
@@ -11,35 +11,5 @@ class LoginController: LoginView {
     func login() {
         let presenter = LoginPresenter(view: self, service: DummyService())
         presenter.login()
-    }
-
-    var credentials: Credentials {
-        return Credentials(username: username, password: password)
-    }
-
-    func showUsernameIsEmpty() {
-        print("Username Is Empty")
-    }
-
-    func showPasswordIsEmpty() {
-        print("Password Is Empty")
-    }
-
-    func showCredentialsAreValid() {
-        print("Credentials Are Valid")
-        let controller = MainController()
-        controller.display()
-    }
-
-    func showCredentialsAreNotValid() {
-        print("Credentials Are Not Valid")
-    }
-
-    func showLoadingInterface() {
-        print("Loading")
-    }
-
-    func hideLoadingInterface() {
-        print("Loading Complete")
     }
 }
