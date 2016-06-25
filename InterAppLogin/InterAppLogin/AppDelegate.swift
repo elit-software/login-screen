20f5d33526bb7e6c2f0a7b75bad4b573e1214ff3
tiagomartinho <tiago.martinho@xpeppers.com>
2016-06-25 12:31:57 +0200

    adds InterApp Login UI Tests

--- InterAppLogin/InterAppLogin/AppDelegate.swift
+++ InterAppLogin/InterAppLogin/AppDelegate.swift
@@ -4,6 +4,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        disableAnimationsForTests()
+        return true
+    }
+
+    func disableAnimationsForTests() {
+        if NSProcessInfo.processInfo().environment["animations"] == "0" {
+            UIView.setAnimationsEnabled(false)
+        }
+    }
+    
     func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
         (window?.rootViewController as? ViewController)?.resultLabel.text = url.absoluteString
         return true
--- InterAppLogin/InterAppLoginUITests/InterAppLoginUITests.swift
+++ InterAppLogin/InterAppLoginUITests/InterAppLoginUITests.swift
@@ -1,4 +1,46 @@
 import XCTest
 
 class InterAppLoginUITests: XCTestCase {
+
+    let application = XCUIApplication()
+
+    var loginButton: XCUIElement?
+
+    private var applicationLaunched = false
+
+    override func setUp() {
+        super.setUp()
+        application.launch()
+        loginButton = application.buttons["login"]
+    }
+
+    private func launchApp() {
+        if !applicationLaunched {
+            applicationLaunched = true
+            continueAfterFailure = false
+            application.launchEnvironment = ["animations": "0"]
+            application.launch()
+            sleep(1)
+        }
+    }
+
+    func testLogin() {
+        loginButton?.tap()
+
+        waitForElement("result")
+
+        let result = application.staticTexts.elementMatchingType(.Any, identifier: "result")
+        XCTAssertEqual("InterAppLogin://?message=CredentialsAreValid", result.label)
+    }
+
+    func waitForElement(element: String, WithTimeOut timeout: Double = 60.0) {
+        var elapsed = 0.0
+        while elapsed < timeout {
+            if application.staticTexts.elementMatchingType(.Any, identifier: element).exists {
+                break
+            }
+            elapsed += 1.0
+            sleep(1)
+        }
+    }
 }
