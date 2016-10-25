265544f7e022c5ef411cf837c1cd5a02dece6586
tiagomartinho <tiago.martinho@xpeppers.com>
2016-10-25 12:31:06 +0200

    migrates command line app to swift 3

--- CrossPlatform/Sources/main.swift
+++ CrossPlatform/Sources/main.swift
@@ -1,8 +1,8 @@
-if Process.arguments.count != 3 {
+if CommandLine.arguments.count != 3 {
     print("Usage: login USERNAME PASSWORD")
 } else {
-    let username = Process.arguments[1]
-    let password = Process.arguments[2]
+    let username = CommandLine.arguments[1]
+    let password = CommandLine.arguments[2]
     let controller = LoginController(username: username, password: password)
     controller.login()
 }
