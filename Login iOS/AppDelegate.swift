0a33452d7bd3324b5354342ac905d0ecfde4a46b
Tiago Martinho <t.martinho@icloud.com>
2018-08-18 12:13:19 +0200

    Format code

--- Login iOS/AppDelegate.swift
+++ Login iOS/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions
-        launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
         disableAnimationsForTests()
         return true
     }
